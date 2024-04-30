f4f9e56c12af3c918ea9d63759551495380446f9
codaski <amaher.code@gmail.com>
2024-04-30 15:39:40 +0300

    add services as dropdown menu items in header

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -24,14 +24,14 @@ const socialMedia = ref([
 </script>
 
 <template>
-    <div class="bg-white py-3">
+    <div class="bg-primary text-white py-3">
         <div class="md:flex block items-center container px-8 justify-between">
             <div class="text-sm md:text-left text-center">{{ useSettingValue('copyrights') }}</div>
             <ul class="flex items-center gap-6 mt-3 md:mt-0 md:place-content-end place-content-center">
                 <template v-for="item in socialMedia" :key="item.name">
                     <li v-if="item.url">
                         <NuxtLink :to="item.url">
-                            <Icon :name="item.icon" class="w-5 h-5 hover:scale-105 ease-in-out duration-300 hover:text-primary" />
+                            <Icon :name="item.icon" class="w-5 h-5 hover:scale-105 ease-in-out duration-300 hover:opacity-100 opacity-50" />
                         </NuxtLink>
                     </li>
                 </template>
