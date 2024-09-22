7ec005ff10efae16af802f8b29ca33d20375792e
codaski <amaher.code@gmail.com>
2024-09-22 09:41:17 +0000

    prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
         name: 'CSM',
-        port: '4567',
+        port: '3015',
         exec_mode: 'cluster',
         instances: 'max',
         script: './.output/server/index.mjs',
